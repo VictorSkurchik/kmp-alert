06332c6e665de2eb5be8b26991a503eddf8ff2b2
Victor Skurchik <victorskurchik@gmail.com>
2026-09-07 11:58:09 +0300

    feat(iosApp): add native SwiftUI monitoring screen and background BLE
    Adds NSBluetoothAlwaysUsageDescription + bluetooth-central background mode, a manual Kotlin-Flow-to-AsyncStream bridge (SKIE bridges suspend->async by default here but not Flow-typed properties), and a native scan/monitoring SwiftUI screen consuming MonitoringViewModel.

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -1,10 +1,16 @@
 import SwiftUI
+import SharedLogic
 
 @main
 struct iOSApp: App {
+    private let appContainer = AppContainer()
+
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(appContainer: appContainer)
+                .task {
+                    _ = try? await appContainer.notificationPermissionManager.requestPermission()
+                }
         }
     }
 }
